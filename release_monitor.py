c1c91f499e13b34ef61291a57ab8e215e4690b12
Tomáš Hrčka <thrcka@redhat.com>
2018-09-26 08:17:02 +0200

    Add configurable interval between scanning of RSS feed

--- release_monitor.py
+++ release_monitor.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 import json
 import logging
-import sys
 import os
-import feedparser
-import requests
 import signal
+import sys
 import time
+
+import feedparser
 import psutil
+import requests
 from f8a_worker.setup_celery import init_celery, init_selinon
 from selinon import run_flow
 
 from defaults import NPM_URL, PYPI_URL, ENABLE_SCHEDULING, \
-    SCHEDULED_NPM_PACKAGES, SCHEDULED_PYPI_PACKAGES, PROBE_FILE_LOCATION
+    PROBE_FILE_LOCATION, SLEEP_INTERVAL
 
 logger = logging.getLogger(__name__)
 
@@ -23,7 +24,7 @@ def handler(signum, frame):
         run_flow('livenessFlow', [None])
     else:
         logger.debug("Liveness probe - livenessFlow"
-                       " did not run since selinon is not initialized")
+                     " did not run since selinon is not initialized")
 
     basedir = os.path.dirname(PROBE_FILE_LOCATION)
     if not os.path.exists(basedir):
@@ -54,22 +55,8 @@ def write_package_info_to_file(node_args, file_path):
     return True
 
 
-def get_correct_ecosystem_filepath(ecosystem):
-    if ecosystem == 'npm':
-        return SCHEDULED_NPM_PACKAGES
-    if ecosystem == 'pypi':
-        return SCHEDULED_PYPI_PACKAGES
-
-
 def was_package_processed(ecosystem, name, version):
-    ecosystem_file = get_correct_ecosystem_filepath(ecosystem)
-    with open(ecosystem_file, "r") as f:
-        print(f.readlines())
     return False
-
-
-def not_in_previous_set():
-    return True
 
 
 class ReleaseMonitor():
@@ -82,6 +69,10 @@ class ReleaseMonitor():
         logging_handler.setFormatter(formatter)
         self.log.addHandler(logging_handler)
         self.log.level = logging.DEBUG
+        self.old_npm_feed = None
+        self.npm_feed = feedparser.parse(NPM_URL + "-/rss")
+        self.old_pypi_feed = None
+        self.pypi_feed = feedparser.parse(PYPI_URL + "rss/updates.xml")
 
         if ENABLE_SCHEDULING:
             init_celery(result_backend=False)
@@ -102,35 +93,40 @@ class ReleaseMonitor():
             'recursive_limit': 0
         }
 
-        write_package_info_to_file(node_args, get_correct_ecosystem_filepath(ecosystem))
         self.log.info("Scheduling Selinon flow '%s' with node_args: '%s'", 'bayesianFlow', node_args)
         return run_flow('bayesianFlow', node_args)
 
+    def entry_not_in_previous_npm_set(self, entry):
+        if entry in self.old_npm_feed:
+            return False
+        else:
+            return True
+
+    def entry_not_in_previous_pypi_set(self, entry):
+        if entry in self.old_pypi_feed:
+            return False
+        else:
+            return True
+
     def run(self):
-        self.log.info("Initializing RSS feeds")
-        feed_pypi = feedparser.parse(PYPI_URL + "rss/updates.xml")
-        feed_npm = feedparser.parse(NPM_URL + "-/rss")
-
-        signal.signal(signal.SIGUSR1, handler)
-        self.log.info("Registered signal handler for liveness probe")
-
         while True:
-            for i in feed_npm.entries:
+            for i in self.npm_feed.entries:
                 package_name = i['title']
                 package_url = NPM_URL + "-/package/{package_name}/dist-tags".format(package_name=package_name)
                 package_latest_version = json.loads(
                     requests.get(package_url, headers={'content-type': 'application/json'}).text)
-                self.log.info("Processing package from npm: '%s':'%s'", package_name, package_latest_version.get('latest'))
-                if ENABLE_SCHEDULING and not_in_previous_set():
+                self.log.info("Processing package from npm: '%s':'%s'", package_name,
+                              package_latest_version.get('latest'))
+                if ENABLE_SCHEDULING and self.entry_not_in_previous_npm_set(i):
                     # self.run_package_analisys(package_name, 'npm', package_latest_version)
                     print({'package_name': package_name,
                            'latest_version': package_latest_version
                            })
 
-            for i in feed_pypi.entries:
+            for i in self.pypi_feed.entries:
                 package_name, package_latest_version = i['title'].split(' ')
                 self.log.info("Processing package from pypi: '%s':'%s'", package_name, package_latest_version)
-                if ENABLE_SCHEDULING:
+                if ENABLE_SCHEDULING and self.entry_not_in_previous_pypi_set(i):
                     # self.run_package_analisys(package_name, 'pypi', package_latest_version)
                     print({'package_name': package_name,
                            'latest_version': package_latest_version
