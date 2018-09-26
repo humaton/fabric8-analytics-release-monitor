abde1abb7eca54f2c0832913790ba209bdb8b0d7
Tomáš Hrčka <thrcka@redhat.com>
2018-09-26 09:32:19 +0200

    Remove unused config options, remove dependencies

--- defaults.py
+++ defaults.py
@@ -4,7 +4,5 @@ NPM_URL = os.environ.get('NPM_URL', 'https://registry.npmjs.org/')
 PYPI_URL = os.environ.get('PYPI_URL', 'https://pypi.org/')
 ENABLE_SCHEDULING = os.environ.get('ENABLE_SCHEDULING', '1') in ('1', 'True', 'true')
 PROBE_FILE_LOCATION = "/tmp/release_monitoring/liveness.txt"
-SCHEDULED_NPM_PACKAGES = "/tmp/release_monitoring/npm.json"
-SCHEDULED_PYPI_PACKAGES = "/tmp/release_monitoring/pypi.json"
 SLEEP_INTERVAL = os.environ.get('SLEEP_INTERVAL', 20)
 
